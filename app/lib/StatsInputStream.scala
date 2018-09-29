fb4ea2b5a8b14062c329d1dac56504004e218fe2
kolena <kolena@avast.com>
2018-09-29 23:51:37 +0200

    StatsInputStream floating window

--- app/lib/StatsInputStream.scala
+++ app/lib/StatsInputStream.scala
@@ -1,25 +1,28 @@
 package lib
 
 import java.io.InputStream
+import java.time.Clock
 import java.util.concurrent.atomic.{AtomicInteger, AtomicReference}
 
 import monix.execution.Scheduler
 
 import scala.concurrent.duration._
 
-class StatsInputStream(input: InputStream)(sch: Scheduler) extends InputStream {
+class StatsInputStream(input: InputStream, clock: Clock = Clock.systemDefaultZone())(sch: Scheduler) extends InputStream {
 
   private val stats = new AtomicReference[(Long, Double)]((0, 0))
 
-  // TODO floating window
-  private val prevSecondBytes = new AtomicInteger(0)
-  private val lastSecondBytes = new AtomicInteger(0)
+  private val currentSecondIndex = new AtomicInteger(0)
+  private val secondsStats = Array.fill(5)(0)
 
   private val scheduled = sch.scheduleAtFixedRate(1.second, 1.second) {
-    // use prev for report, set current as prev, reset current
-    val secondFinal = prevSecondBytes.getAndSet(lastSecondBytes.getAndSet(0)) / 1000.0
+    val secondIndex = currentSecondIndex.getAndUpdate(i => (i + 1) % 5)
 
-    stats.accumulateAndGet((0, secondFinal), (oldData: (Long, Double), newData: (Long, Double)) => {
+    val unrolled = secondsStats.slice(secondIndex, 5) ++ secondsStats.slice(0, secondIndex - 1)
+
+    val speed = unrolled.zipWithIndex.map { case (b, i) => b * ((i + 1) / 10.0) }.sum / 1000.0
+
+    stats.accumulateAndGet((0, speed), (oldData: (Long, Double), newData: (Long, Double)) => {
       (oldData._1, newData._2)
     })
   }
@@ -40,7 +43,8 @@ class StatsInputStream(input: InputStream)(sch: Scheduler) extends InputStream {
     stats.accumulateAndGet((uploadedBytes, 0), (oldData: (Long, Double), newData: (Long, Double)) => {
       (oldData._1 + newData._1, oldData._2)
     })
-    lastSecondBytes.addAndGet(uploadedBytes)
+
+    secondsStats(currentSecondIndex.get()) += uploadedBytes
   }
 
   def snapshot: (Long, Double) = stats.get()
