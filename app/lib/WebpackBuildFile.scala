f3e3ce4bb758398cd4fe2ba27569f6727eb6f418
kolena <kolena@avast.com>
2018-10-29 22:25:28 +0100

    Windows deploy config

--- app/lib/WebpackBuildFile.scala
+++ app/lib/WebpackBuildFile.scala
@@ -1,7 +1,7 @@
 package lib
 
 import java.net.URI
-import java.nio.file.{FileSystems, Files}
+import java.nio.file.{FileSystems, Files, Paths}
 import java.util
 
 import com.typesafe.scalalogging.StrictLogging
@@ -10,12 +10,16 @@ import scala.collection.JavaConverters._
 
 object WebpackBuildFile extends StrictLogging {
 
-  private val uri: URI = getClass.getClassLoader.getResource("public/bundle").toURI
-  logger.debug(s"Found bundle resources @ $uri")
+  private val bundleDir = if (Files.exists(Paths.get("public", "bundle"))) {
+    Paths.get("public", "bundle")
+  } else {
+    val uri: URI = getClass.getClassLoader.getResource("public/bundle").toURI
+    logger.debug(s"Found bundle resources @ $uri")
 
-  private val uriParts: Array[String] = uri.toString.split("!")
-  private val fs = FileSystems.newFileSystem(URI.create(uriParts.head), new util.HashMap[String, Object]())
-  private val bundleDir = fs.getPath(uriParts(1))
+    val uriParts: Array[String] = uri.toString.split("!")
+    val fs = FileSystems.newFileSystem(URI.create(uriParts.head), new util.HashMap[String, Object]())
+    fs.getPath(uriParts(1))
+  }
 
   logger.info(s"Locating built bundle at ${bundleDir.toUri}")
 
