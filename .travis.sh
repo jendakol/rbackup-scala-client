87716b2755918fe7208c09f75e314e12a3b3cee7
kolena <kolena@avast.com>
2018-12-14 20:20:21 +0100

    Tests fix

--- .travis.sh
+++ .travis.sh
@@ -31,8 +31,11 @@ function client_test() {
      docker-compose down
 }
 
+dir=$(pwd)
+
 client_test &&
   if $(test "${TRAVIS_REPO_SLUG}" == "jendakol/rbackup-scala-client" && test "${TRAVIS_PULL_REQUEST}" == "false" && test "$TRAVIS_TAG" != ""); then
+    cd $pwd
     sbt +publish
   else
     exit 0 # skipping publish, it's regular build
